217208b36c9a74db6c40778cb7a79c7dd3bd2911
K-wanC <kwanchitng@gmail.com>
2026-08-02 23:58:13 -0400

    Change tone frequencies for Mini Piano
    Updated tone frequencies for button presses to use new notes.

--- Button Piano/Mini_Piano.ino
+++ Button Piano/Mini_Piano.ino
@@ -1,12 +1,11 @@
-#include "C:\Users\Kwan Chit Ng\Desktop\wkz\coding\Arduino\Mini_Piano\tonechart.txt"
-
+#include "...\tone.chart.txt"
+//Should include full directory to the file
 int pin1 = 2;
 int pin2 = 3;
 int pin3 = 4;
 int pin4 = 5;
 int speaker = 10;
 void setup() {
-  // put your setup code here, to run once:
   pinMode(pin1,INPUT_PULLUP);
   pinMode(pin2,INPUT_PULLUP);
   pinMode(pin3,INPUT_PULLUP);
@@ -15,21 +14,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Utilizing the tone function, as it can alternate hertz and time of the output, the notes were defined in the attachment
   while(digitalRead(pin1)==LOW)
   {
-    tone(speaker,NOTE_E2,10);
+    tone(speaker,NOTE_C1,10);
   }
   while(digitalRead(pin2)==LOW)
   {
-    tone(speaker,NOTE_A2,10);
+    tone(speaker,NOTE_C2,10);
   }
   while(digitalRead(pin3)==LOW)
   {
-    tone(speaker,NOTE_D1,10);
+    tone(speaker,NOTE_C3,10);
   }
   while(digitalRead(pin4)==LOW)
   {
-    tone(speaker,NOTE_G1,10);
+    tone(speaker,NOTE_C4,10);
   }
 }
